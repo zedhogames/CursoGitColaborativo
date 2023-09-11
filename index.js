167cd179a0c37014d02ac6f47e79072b3f57aad0
serge <sergiozanella21@gmail.com>
2023-09-11 00:16:45 -0300

    Actualizado js

--- index.js
+++ index.js
@@ -7,4 +7,21 @@ let provincia = 'Buenos aires';
 let pais = 'Argentina';
 let fecha = 2023;
 
-console.log('Buenas, soy ' + nombre + ' de la provincia de ' + provincia + ', ');
+console.log('Buenas, soy ' + nombre + ' de la provincia de ' + provincia + ', '+ pais);
+
+console.log("***********************************************************");
+
+
+function sumar(){
+    
+    let numeroUno = 10;
+    let numeroDos = 5;
+
+    let resultado = numeroDos + numeroUno;
+    
+    console.log('El resultado es ${resultado}');
+
+
+}
+
+sumar();
