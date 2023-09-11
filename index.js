f979af9961e26df0b199c22574b9c5b5a439e4e0
serge <sergiozanella21@gmail.com>
2023-09-10 23:28:26 -0300

    va el archivo index etapa inicial

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('Hola mundo de Git');
+console.log('Hola mundo de Git');
+console.log('Hola mundo de Git');
+
+let nombre = 'Sergio';
+let provincia = 'Buenos aires';
+let pais = 'Argentina';
+let fecha = 2023;
+
+console.log('Buenas, soy ' + nombre + ' de la provincia de ' + provincia + ', ');
